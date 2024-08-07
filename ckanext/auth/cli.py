8105e213ab1fbcbbe7b0028e855fc3d2d006c9de
mutantsan <kvaqich@gmail.com>
2024-08-07 11:06:46 +0300

    add get-code command for dev purposes

--- ckanext/auth/cli.py
+++ ckanext/auth/cli.py
@@ -4,7 +4,7 @@ import logging
 
 import click
 
-from ckanext.auth import utils
+from ckanext.auth import utils, model
 
 logger = logging.getLogger(__name__)
 
@@ -33,3 +33,14 @@ def unblock_user(username):
 @auth.command()
 def unblock_all():
     utils.LoginManager.reset_all()
+
+
+@auth.command()
+@click.argument("username")
+def get_code(username):
+    secret = model.UserSecret.get_for_user(username)
+
+    if not secret:
+        secret = model.UserSecret.create_for_user(username)
+
+    click.secho(f"Secret: {secret.get_code()}", fg="green")
